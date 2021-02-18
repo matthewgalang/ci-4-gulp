d8c77dcd7030e422869b1250702ff2d540b9bc32
Matt Galang <magalang1@gmail.com>
2021-02-18 11:08:30 -0800

    Refactor css transforms directory

--- Gulpfile.js
+++ Gulpfile.js
@@ -16,6 +16,11 @@ var gulp = require("gulp"),
 var isProduction = mode.production();
 const PostCssPlugins = [
     require("postcss-modules")({
+        getJSON: function (cssFileName, json) {
+            var cssName = path.basename(cssFileName, ".css");
+            var jsonFileName = path.resolve('scss/transforms/' + cssName + ".json");
+            fs.writeFileSync(jsonFileName, JSON.stringify(json));
+        },
         generateScopedName: isProduction
             ? "[contenthash:base64:5]m4s"
             : "[name]__[local]___[contenthash:base64:5]",
--- app/Libraries/CSS/CSSModules.php
+++ app/Libraries/CSS/CSSModules.php
@@ -9,19 +9,19 @@ namespace App\Libraries\CSS;
 
 class CSSModules
 {
-   private $path;
+   private $transformation_path;
    private $file_manifest;
    public $manifest_json;
 
    public function __construct() {
-      $this->path = ROOTPATH . 'scss/';
+      $this->transformation_path = ROOTPATH . 'scss/transforms/';
       $this->file_manifest = file_get_contents(ROOTPATH . "/scss/manifest.json");
       $this->manifest_json = json_decode($this->file_manifest, true);
    }
    public function getClasses(string $file): array
    {
       // get styles manifest
-      $file_glob = rsearch($this->path,"/^.*".$file."\.css.json$/");
+      $file_glob = rsearch($this->transformation_path,"/^.*".$file."\.json$/");
       $json = file_get_contents($file_glob[0]);
       $classes = json_decode($json);
 
